d1879d8864891497664e167b2d48513ca5dc073f
muhammadkashifzia <kashif@idenbrid.com>
2025-06-05 17:12:28 +0500

    code push

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -9,22 +12,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next', 'next/core-web-vitals', 'prettier'],
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'warn',
-      'no-var': 'error',
-      'no-multiple-empty-lines': ['error', { max: 1 }],
-      curly: ['error', 'all'],
-      semi: ['error'],
-      quotes: ['error', 'single'],
-      'prefer-arrow-callbacks': ['error'],
-      'prefer-template': ['error'],
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  }),
-];
+const eslintConfig = [...compat.config({
+  extends: ['next', 'next/core-web-vitals', 'prettier'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'no-var': 'error',
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    curly: ['error', 'all'],
+    semi: ['error'],
+    quotes: ['error', 'single'],
+    'prefer-arrow-callbacks': ['error'],
+    'prefer-template': ['error'],
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
+  },
+}), ...storybook.configs["flat/recommended"]];
 export default eslintConfig;
