b088dcd791b7a1a384fa14391ba47e363352a6fd
muhammadkashifzia <kashif@idenbrid.com>
2025-06-18 15:19:59 +0500

    code push

--- app/src/components/hometwo/HeroSlider.jsx
+++ app/src/components/hometwo/HeroSlider.jsx
@@ -1,9 +1,6 @@
 // components/HeroSlider.js
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
+cccccc
 
 const HeroSlider = () => {
   return (
