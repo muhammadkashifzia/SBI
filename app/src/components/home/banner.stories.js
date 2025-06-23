c304c310614a6d878cd2a51cf613fb04d09b9221
muhammadkashifzia <kashif@idenbrid.com>
2025-06-23 12:28:32 +0500

    code push

--- app/src/components/home/banner.stories.js
+++ app/src/components/home/banner.stories.js
@@ -1,7 +1,16 @@
-import Banner from "./banner.jsx";
+import React from 'react';
+import Banner from '@/components/home/banner';
+
 export default {
-  title: 'Home Page Banner Find a Store',
-  component: Banner
+  title: 'Components/Home Page Banner',
+  component: Banner,
+  args: {
+    title1: "無料で店舗登録",
+    title2: "最短３分!",
+    buttonText: "店舗を掲載して集客スタート",
+    backgroundImage: "/assets/images/register-store-banner02.jpg",
+    bannerLinkUrl: "/restaurants",
+  },
 };
 
-export const HomeBanner = {};
+export const HomeBanner = (args) => <Banner {...args} />;
