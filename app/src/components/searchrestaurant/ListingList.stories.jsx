1d486462f64d5bf1bb854ecfff86e8f37cf78dbb
muhammadkashifzia <kashif@idenbrid.com>
2025-06-23 14:14:52 +0500

    code push

---
+++ app/src/components/searchrestaurant/ListingList.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ListingList from '@/components/searchrestaurant/ListingList';
+
+export default {
+  title: 'SearchRestaurant/ListingList',
+  component: ListingList,
+};
+
+export const Default = () => <ListingList />;
--- app/src/components/searchrestaurant/listingItemCard.stories.js
+++ app/src/components/searchrestaurant/listingItemCard.stories.js
@@ -1,7 +1,24 @@
-import ListingItemCard from "./ListingItemCard.jsx";
+import React from 'react';
+import ListingItemCard from './ListingItemCard';
+
 export default {
-  title: 'Search List Item Card',
-  component: ListingItemCard
+  title: 'Search Restaurant Listing Item Card',
+  component: ListingItemCard,
 };
 
-export const ListingItem = {};
+const Template = (args) => <ListingItemCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  id: '1',
+    title: 'インペリアブッフェ - 帝国ホテルレストラン - ',
+    price: 5000,
+    rating: 5,
+    category: '和食 / グルメ',
+    station: '新宿駅 徒歩2分',
+    tagline: '美味しさと雰囲気が自慢。',
+    description:
+      'こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になりますこちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります。こちらは店舗の説明になります',
+    image: '/assets/images/restaurant-image.png',
+    reviews: 20,
+};
