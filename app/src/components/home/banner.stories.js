47744c7b56d07ddd56b50ef77e40a29a599c8f81
muhammadkashifzia <kashif@idenbrid.com>
2025-06-23 12:31:39 +0500

    code push

--- app/src/components/home/banner.stories.js
+++ app/src/components/home/banner.stories.js
@@ -5,11 +5,11 @@ export default {
   title: 'Components/Home Page Banner',
   component: Banner,
   args: {
-    title1: "無料で店舗登録",
-    title2: "最短３分!",
-    buttonText: "店舗を掲載して集客スタート",
-    backgroundImage: "/assets/images/register-store-banner02.jpg",
-    bannerLinkUrl: "/restaurants",
+    title1: "全国で掲載店舗拡大中",
+    title2: "お得に使用できる店舗を探そう",
+    buttonText: "掲載店舗を探す",
+    backgroundImage: "/assets/images/banner.png",
+    bannerLinkUrl: "/home",
   },
 };
 
