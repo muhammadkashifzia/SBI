5eb60f9525b8a1893ac37671d8748b0d23d3aa49
muhammadkashifzia <kashif@idenbrid.com>
2025-06-23 16:11:37 +0500

    code push

---
+++ app/src/app/auth/forgot-password/forgot.stories.js
@@ -0,0 +1,7 @@
+import ForgotForm from "@/app/auth/forgot-password/page.jsx";
+export default {
+  title: 'Forgot Password Form',
+  component: ForgotForm,
+};
+
+export const ForgotFormPage = {};
---
+++ app/src/app/auth/login/login.stories.js
@@ -0,0 +1,7 @@
+import LoginForm from "@/app/auth/login/page.jsx";
+export default {
+  title: 'Login Form',
+  component: LoginForm,
+};
+
+export const LoginFormPage = {};
